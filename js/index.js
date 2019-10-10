eb65c6e660f410bc594f7776b3f57acdcec5cd57
Edward <edglock@gmail.com>
2019-10-10 20:01:58 +0000

    devide by zero detection

--- js/index.js
+++ js/index.js
@@ -8,11 +8,24 @@ function btnClicked(ev) {
 
 //document.querySelector('#cancel')
 
-//document.querySelector('#equal').addEventListener('click' ,
-//() => display.value = eval(display.value));
+
 document.querySelector('#equal').addEventListener('click', evaluation);
 function evaluation(){
-    //let zero = display.value.length;
-    //display.value = display.value.slice(zero-2, zero);
+    devideByZeroDetection();
     display.value = eval(display.value);
+}
+
+function devideByZeroDetection() {
+    let slashPosition = -1;
+    let slashCount = 0;
+    let arrayForPosition = [];
+    do {
+	    slashPosition = display.value.indexOf("/0", slashPosition + 1);
+        if (slashPosition !== -1) arrayForPosition.push(slashPosition);
+    }
+    while (slashPosition !== -1);
+    for(let i = 0; i < arrayForPosition.length; i++){
+	    if(display.value[arrayForPosition[i] + 2] == ".") slashCount++;
+    }
+    if (arrayForPosition.length > slashCount) display.value = "error: devide by zero";
 }
