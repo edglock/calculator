c8e4efcbda8d8ec355e991225717b8e803c52fec
Edward <edglock@gmail.com>
2019-10-10 14:26:31 +0000

    calculator created

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-const link = document.querySelector('a');
+const display = document.querySelector('#display');
+document.querySelectorAll('#numpad button')
+    .forEach(btn => btn.addEventListener('click', btnClicked));
+
+function btnClicked(ev) {
+    display.value += ev.target.innerText;
+}
+
+document.querySelector('.equal').addEventListener('click' ,
+  () => display.value = eval(display.value));
+
